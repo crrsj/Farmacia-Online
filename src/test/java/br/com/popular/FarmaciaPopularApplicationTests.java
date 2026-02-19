7da45ccb5dbdf889a09ab30aefcba02ac4fe843f
Carlos Roberto Ribeiro Santos Junior <138411850+crrsj@users.noreply.github.com>
2026-02-18 22:08:15 -0300

    Update FarmaciaPopularApplicationTests.java

--- src/test/java/br/com/popular/FarmaciaPopularApplicationTests.java
+++ src/test/java/br/com/popular/FarmaciaPopularApplicationTests.java
@@ -4,6 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API - Farmácia Online.",
+				version = "1.0",
+				description = " Sistema voltado para venda de remédios online.",
+				contact = @Contact()
+		)
+)	
 class FarmaciaPopularApplicationTests {
 
 	@Test
